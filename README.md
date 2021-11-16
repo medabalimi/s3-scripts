7d8412863a1821423fa7f2ab479c28d4981969f6
medabalimi <joseph.anand@gmail.com>
2021-11-16 05:44:48 +0530

    Rebase commits to current version. Added threading support and made changes to function on Windows.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
+# AWS Policies
+1. s3upload.json: Policy to provide access to an S3 bucket to a particular IAM role
+2. cfinvalidate.json: Policy to provide access to an IAM role to invalidate a cloudfront cache
+
 # s3-scripts
+1. push2S3CFInv.py: push files to s3 bucket (policy: s3upload.json) and invalidate cache (policy: cfinvalidate.json). Requires config file in json format to be passed as the first argument. ```{
+             "iam":
+                  {"id": "S3_IAM_ID",
+                   "key": "S3_IAM_KEY"
+                  },
+             "s3":
+                  {
+                   "bucket": "S3_BUCKET",
+                   "region": "S3_REGION"
+                  },
+             "cloudfront": 
+                 {
+                 "id": "CLOUDFRONT_DISTRIBUTION_ID"
+                 }
+            }```
--- push2s3Cinv.py
+++ push2s3Cinv.py
@@ -8,16 +8,19 @@ Created on Sat Nov  6 07:41:54 2021
 
 import boto3
 import time
-import os, sys
+import os
+import sys
 
 import click
 import json
 import mimetypes
 
+from multiprocessing.pool import ThreadPool
+
 
 
 class Deploy2S3:
-    def __init__(self, conf:dict):
+    def __init__(self, conf:dict, threads):
         """
       
 
@@ -48,11 +51,13 @@ class Deploy2S3:
         """
         
         self.conf=conf
+        print(conf["iam"]["id"], conf["iam"]["key"])
         self.s3_session = boto3.Session(aws_access_key_id=conf["iam"]["id"],
                                    aws_secret_access_key=conf["iam"]["key"], 
                                    region_name=conf["s3"]["region"])
         self.s3 = self.s3_session.resource('s3')
         self.bucket = self.s3.Bucket(conf["s3"]["bucket"])
+        self.threads = threads
  
     def push_to_s3(self, folder):
         """
@@ -67,20 +72,45 @@ class Deploy2S3:
         None.
 
         """
+        if not os.path.isfile(folder):
+            file_paths= self.get_files(folder)
+            pool = ThreadPool(processes=self.threads)
+            pool.map(self.upload,file_paths)
+        else:
+            self.upload_file(folder)
+    
+    def get_files(self, folder):
         for s_dir, dirs, files in os.walk(folder):
-            for file in files:
-                file_path = os.path.join(s_dir, file)
-                print(f"Uploading {file_path} to s3://{self.conf['s3']['bucket']}")
-                s3_args={'ACL': 'public-read'}
-                content_type= mimetypes.guess_type(file_path)[0]
-                if content_type is not None:
-                    s3_args['ContentType'] = content_type
+                print(folder)
+                for file in files:
+                    print(file)
+                    file_path = os.path.join(s_dir, file)
+                    s3_path=file_path[len(folder):]
+                    if sys.platform.startswith('win32'):
+                        s3_path=s3_path.replace('\\','/')
+                    s3_path=s3_path.lstrip('/')
+                    yield file_path, s3_path
     
-                self.bucket.upload_file(file_path,
-                                        file_path[len(folder):],
-                                        ExtraArgs=s3_args)
+    def upload(self, file_paths):
+        src_file_path,s3_path=file_paths
+        self.upload_file(src_file_path,s3_path)
+        
+    def upload_file(self, src_file_path, s3_path=None):
+        s3_args={} 
+        if s3_path is None:
+            
+            s3_path=src_file_path.split('/')[-1]
+        content_type= mimetypes.guess_type(src_file_path)[0]
+        if content_type is not None:
+            s3_args['ContentType'] = content_type
+                
+        print(f"Uploading {src_file_path} to s3://{self.conf['s3']['bucket']}")
+        #print(src_file_path,s3_path,s3_args)
 
-    
+        self.bucket.upload_file(src_file_path,
+                                s3_path,
+                                ExtraArgs=s3_args)
+        
     def invalidate_distribution(self):
         cf=boto3.client('cloudfront', 
                         aws_access_key_id=self.conf["iam"]["id"],
@@ -99,6 +129,9 @@ class Deploy2S3:
 
 @click.command()
 @click.option('--config', '-c', required=True, type=click.File('rb'))
+@click.option('--threads','-t', default=5, type=int)
+@click.option('--cf-inv/--skip-cf-inv', default=True)
+@click.option('--s3-push/--skip-s3-push', default=True)
 @click.argument('folder',  required=True,
                 type=click.Path(exists=True, 
                                 file_okay=True, 
@@ -106,11 +139,15 @@ class Deploy2S3:
                                 readable=True
                                ))
 
-def main(config, folder):
-    s3deploy=Deploy2S3(json.load(config))
-    print("Uploading files to s3")
-    s3deploy.push_to_s3(folder)
-    print(f"Invalidating cloudfront cache - ID: {s3deploy.invalidate_distribution()}")
+def main(config, cf_inv,s3_push, threads, folder):
+    
+    s3deploy=Deploy2S3(json.load(config), threads)
+    if s3_push:
+        print("Uploading files to s3")
+        s3deploy.push_to_s3(folder)
+    if cf_inv:
+        print(f"Invalidating cloudfront cache - ID: {s3deploy.invalidate_distribution()}")
+        s3deploy.invalidate_distribution()
 
 if __name__ == "__main__":
     main()
